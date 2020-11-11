e274fc1f0c55774e4e4bf3c670e66103da3f087f
sorok007 <sorok007@mail.ru>
2020-11-11 22:06:50 +0300

    test 1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <QCoreApplication>
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+
+
+
+    cout<<"test git1"<<endl;
+
+    return a.exec();
+}
